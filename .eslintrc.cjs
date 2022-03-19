53ae30c77f0eb6ad05505fc446d7f4d69e5b1c20
Teddy Byron <ted@tedbyron.com>
2022-03-19 00:19:19 -0400

    fix config
    Signed-off-by: Teddy Byron <ted@tedbyron.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['prettier', 'standard-with-typescript'],
   plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs'],
+  ignorePatterns: ['*.cjs', '/.yarn/'],
   overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
   settings: {
     'svelte3/typescript': () => require('typescript')
@@ -18,4 +18,4 @@ module.exports = {
     es2017: true,
     node: true
   }
-};
+}
