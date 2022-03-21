6d2ff67d078054c749ad6671d9a9d4e6efda3f46
Teddy Byron <ted@tedbyron.com>
2022-03-21 10:24:13 -0400

    update tsconfig
    Signed-off-by: Teddy Byron <ted@tedbyron.com>

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const { TODO_SUPABASE_KEY, TODO_SUPABASE_URL } = import.meta.env
 
 if (typeof TODO_SUPABASE_KEY !== 'string' || typeof TODO_SUPABASE_URL !== 'string') {
-  throw new Error('Invalid environment variables')
+  throw new Error('Missing or invalid environment variables')
 }
 
 const supabase = createClient(TODO_SUPABASE_URL, TODO_SUPABASE_KEY)
