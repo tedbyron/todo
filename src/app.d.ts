41ec555882cf9b10e8e2127adc51213f13462c73
Teddy Byron <ted@tedbyron.com>
2022-03-22 15:43:21 -0400

    fix env var types
    Signed-off-by: Teddy Byron <ted@tedbyron.com>

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,1 +1,12 @@
 declare module '@primer/octicons'
+
+declare interface ImportMetaEnv {
+  readonly TODO_SUPABASE_KEY: string
+  readonly TODO_SUPABASE_URL: string
+  readonly TODO_GH_CLIENT_ID: string
+  readonly TODO_GH_CLIENT_SECRET: string
+}
+
+declare interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
